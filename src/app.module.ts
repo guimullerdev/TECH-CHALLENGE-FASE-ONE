6dd69126f071b9edf7f00291a2fe301d1229745b
Guilherme Müller <54514054+guimullerdev@users.noreply.github.com>
2026-04-16 23:35:51 -0300

    imported module auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
 import { CustomersModule } from './modules/customers/customers.module';
 import { VehiclesModule } from './modules/vehicles/vehicles.module';
 import { ServicesModule } from './modules/services/presentation/services.module';
@@ -15,10 +16,10 @@ import { ServiceOrdersModule } from './modules/service-orders/service-orders.mod
     }),
 
     PrismaModule,
+    AuthModule,
     ServiceOrdersModule,
     CustomersModule,
     VehiclesModule,
-
     ServicesModule,
     PartsModule,
   ],
